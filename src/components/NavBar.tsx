b63167a80973a198bcf1ea97a37e36cf7176e2c0
Jayden Lefebvre <jayden.lefebvre55@gmail.com>
2021-08-16 17:38:21 -0400

    Added Logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,15 +4,15 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import InputBase from '@material-ui/core/InputBase';
-import Badge from '@material-ui/core/Badge';
+// import InputBase from '@material-ui/core/InputBase';
+// import Badge from '@material-ui/core/Badge';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
-import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
+// import MenuIcon from '@material-ui/icons/Menu';
+// import SearchIcon from '@material-ui/icons/Search';
 import AccountCircle from '@material-ui/icons/AccountCircle';
-import MailIcon from '@material-ui/icons/Mail';
-import NotificationsIcon from '@material-ui/icons/Notifications';
+// import MailIcon from '@material-ui/icons/Mail';
+// import NotificationsIcon from '@material-ui/icons/Notifications';
 import MoreIcon from '@material-ui/icons/MoreVert';
 import { useAuth } from '../contexts/AuthContext';
 import firebase from 'firebase/app';
@@ -122,10 +122,10 @@ export default function PrimarySearchAppBar() {
       onClose={handleMenuClose}
     >
       {useAuth() ? (
-        <MenuItem onClick={()=>{firebase.auth().signOut()}}>Sign Out</MenuItem>
-      ) : (
-        <MenuItem component={Link} to="/login">Sign In</MenuItem>
-      )}
+        <MenuItem onClick={()=>{firebase.auth().signOut(); handleMenuClose()}}>Sign Out</MenuItem>
+      ) : 
+      (<MenuItem component={Link} to="/login">Sign In</MenuItem>)
+      }
     </Menu>
   );
 
